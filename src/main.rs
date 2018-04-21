d8795377667815b5ee95484217610c6966d4e93f
kavya <soldatargent@gmail.com>
2018-04-21 20:25:04 +0530

    added basic code to search one file for a particular string

--- src/main.rs
+++ src/main.rs
@@ -3,34 +3,46 @@
  * Program which takes a file and a regex as argument and outputs the lines containing regex.
  */
 
-extern crate docopt;
-extern crate serde;
-#[macro_use]
-extern crate serde_derive;
+use std::io::BufReader;
+use std::io::prelude::*;
+use std::fs::File;
+use std::env;
 
-use docopt::Docopt;
-
-const USAGE: &'static str = "
-Congrep.
-
-Usage:
-    ./congrep -i <input> -r <regex>
-
-Options:
--h --help   Show this screen
---version   Show version
--i --input  Input file name/stdin
--r --regex  Regex to match
-";
-
-#[derive(Debug, Deserialize)]
-struct Args {
-    input: String,
-    regex: String,
+pub fn search_file(file_path: String, search_string: &'static str){
+    let mut results: Vec<(String, u32)> = Vec::new();
+    match File::open(file_path) {
+        Ok(f) => {
+            let reader = BufReader::new(f);
+            let lines = reader.lines().map(|l| l.unwrap());
+            let mut enumerate = lines.zip(0..);
+            loop {
+                match enumerate.next() {
+                    Some ((line, num)) => {
+                        if line.as_str().contains(search_string) {
+                            results.push((line, num));
+                        }
+                    },
+                    None => break
+                }
+            }
+        },
+        Err(msg) => {
+            panic!("Cannot open file. {}", msg);
+        }
+    };
+    let mut result_iter = results.into_iter();
+    loop {
+        match result_iter.next() {
+            Some((line, num)) => {
+                println!("{}| {}", num, line);
+            },
+            None => break
+        }
+    };
 }
 
 pub fn main() {
-    /* Get the command line arguments */
-    let args: Args = Docopt::new(USAGE).and_then(|d| d.deserialize()).unwrap_or_else(|e| e.exit());
-    println!("{:?}", args);
+    let args: Vec<_> = env::args().collect();
+    let filename: &String = &args[1];
+    search_file(filename.clone(), "hello");
 }
