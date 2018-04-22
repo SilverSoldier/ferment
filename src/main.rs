c80ad626110d524c2ea16bfb3a866c7dc7ceadf9
kavya <soldatargent@gmail.com>
2018-04-22 10:28:07 +0530

    added color to output. Fixed bug in false lines.

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,13 @@
  * Program which takes a file and a regex as argument and outputs the lines containing regex.
  */
 
+extern crate ansi_term;
+
 use std::io::BufReader;
 use std::io::prelude::*;
 use std::fs::File;
 use std::env;
+use ansi_term::Color::{Green, Blue};
 
 pub fn search_file(file_path: String, needle: &'static str){
     let mut results: Vec<(Vec<String>, u32)> = Vec::new();
@@ -14,23 +17,20 @@ pub fn search_file(file_path: String, needle: &'static str){
         panic!("Unable to open {}: {}", file_path, err)
     });
     for (num, line) in (0..).zip(BufReader::new(file).lines().filter_map(move |res| res.ok())) {
-        // let split = line.split(needle);
-        // let sp: Vec<String> = split.map(|x| x.to_string()).collect();
         let split: Vec<String> = line.split(needle)
-                                     .map(|x| x.to_string())
-                                     .collect();
-        if split.len() != 1 || split[0] != "" {
+            .map(|x| x.to_string())
+            .collect();
+        if split.len() != 1 {
             results.push((split, num));
         }
     }
     let mut result_iter = results.into_iter();
     while let Some((split_string, index)) = result_iter.next() {
-        print!("{}| {:?}", index, split_string);
-        // while let Some(string) = split_string.iter().next() {
-        //     if string != "" {
-        //         print!("{}", string);
-        //     }
-        // }
+        print!("{}| ", Blue.paint(index.to_string()));
+        for i in 0..split_string.len()-1 {
+            print!("{}{}", split_string[i], Green.paint(needle));
+        }
+        println!("{}", split_string[split_string.len()-1])
     }
 }
 
