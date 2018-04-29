a514f50b68afdbcdb3671937d586f0e0ec0e9ac6
kavya <soldatargent@gmail.com>
2018-04-29 19:51:16 +0530

    changed program name to 'ferment'. Added more error handling.

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@
 extern crate ansi_term;
 extern crate getopts;
 
-use ansi_term::Color::{Green, Red, Blue};
+use ansi_term::Color::{Green, Red, Blue, Yellow};
 use getopts::Options;
 use std::env;
 use std::fs::{File, metadata, read_dir};
@@ -20,23 +20,29 @@ pub fn spawn_thread(search_string: &str) -> (Sender<String>, JoinHandle<()>){
     let (tx, rx) = channel();
     let handler: JoinHandle<_> = thread::spawn(move || {
         thread_task(rx, &needle);
-        print!("Hello");
     });
     (tx, handler)
 }
 
 pub fn thread_task(rx: Receiver<String>, search_string: &str){
     loop {
-        let file_name = rx.recv().unwrap();
+        let file_name = match rx.recv() {
+            Ok(f) => f,
+            Err(_) => return
+        };
         search_file(file_name, search_string);
     }
 }
 
 pub fn search_file(file_path: String, needle: &str){
     let mut results: Vec<(Vec<String>, u32)> = Vec::new();
-    let file = File::open(file_path.clone()).unwrap_or_else(|err| {
-        panic!("Unable to open {}: {}", file_path, err)
-    });
+    let file = match File::open(file_path.clone()) {
+        Ok(f) => f,
+        Err(err) => {
+            println!("Unable to open {}: {}", Red.bold().paint(file_path), err);
+            return ;
+        }
+    };
     for (num, line) in (0..).zip(BufReader::new(file).lines().filter_map(move |res| res.ok())) {
         let split: Vec<String> = line.split(needle)
             .map(|x| x.to_string())
@@ -47,7 +53,7 @@ pub fn search_file(file_path: String, needle: &str){
     }
     let mut result_iter = results.into_iter();
     while let Some((split_string, index)) = result_iter.next() {
-        print!("{}:{}: ", Red.paint(file_path.clone()), Green.paint(index.to_string()));
+        print!("{}:{}: ", Yellow.paint(file_path.clone()), Green.paint(index.to_string()));
         for i in 0..split_string.len()-1 {
             print!("{}{}", split_string[i], Blue.bold().paint(needle));
         }
@@ -60,21 +66,27 @@ pub fn print_usage(program: &str, opts: Options){
     println!("{}", opts.usage(&help));
 }
 
-pub fn give_jobs(workers: &Vec<(Sender<String>, JoinHandle<()>)>, mut worker: usize, jobs: Vec<String>, recursive: bool) -> usize {
+pub fn give_jobs(workers: &Vec<(Sender<String>, JoinHandle<()>)>, mut worker: usize, jobs: Vec<String>, recursive: bool, n_threads: usize) -> usize {
     /* TODO: keep hashmap of files done, so as to not search already searched files */
     for file in jobs {
-        let md = metadata(file.clone()).unwrap();
+        let md = match metadata(file.clone()) {
+            Ok(m) => m,
+            Err(msg) => {
+                println!("Error for {}: {}", file, msg);
+                continue;
+            }
+        };
         if md.is_file(){
             let (ref tx, _) = workers[worker];
-            tx.send(file);
-            worker = (worker + 1) % 5;
+            tx.send(file).unwrap();
+            worker = (worker + 1) % n_threads;
         }
         else if md.is_dir() && !recursive {
             println!("ferment: {}: Is a directory", Red.bold().paint(file));
         }
         else if md.is_dir() {
             let paths = read_dir(file).unwrap().map(|p| String::from(p.unwrap().path().to_str().unwrap())).collect();
-            worker = give_jobs(&workers, worker, paths, recursive);
+            worker = give_jobs(&workers, worker, paths, recursive, n_threads);
         }
     }
     worker
@@ -85,24 +97,26 @@ pub fn main() {
     let program = args[0].clone();
     let mut opts = Options::new();
     opts.optflag("h", "help", "print this help menu");
-    opts.optflag("R", "recursive", "recursively search directories");
+    opts.optflag("r", "recursive", "read all files under each directory recursively.");
     opts.optopt("j", "jobs", "number of cores to use. If unspecified, assumes 1 core.", "JOBS");
     let mut matches = match opts.parse(&args[1..]) {
-        Ok(m) => { m }
-        Err(f) => panic!(f.to_string())
+        Ok(m) => { m },
+        Err(f) => {
+            println!("{}.", f);
+            print_usage(&program, opts);
+            return;
+        }
     };
-    if matches.opt_present("h") {
+
+    if matches.opt_present("h") || matches.free.len() < 2 {
         print_usage(&program, opts);
         return;
-    }
+    };
+
+    let recursive = matches.opt_present("r");
     let n_threads = match matches.opt_str("j") {
-        Some(n) => n.parse::<i32>().unwrap(),
+        Some(n) => n.parse::<usize>().unwrap(),
         None => 1
-    };
-    let recursive = matches.opt_present("R");
-    if matches.free.len() < 2 {
-        print_usage(&program, opts);
-        return;
     };
 
     let search_string = matches.free.remove(0);
@@ -110,10 +124,14 @@ pub fn main() {
     let mut workers = Vec::with_capacity(n_threads as usize);
 
     /* Spawn one thread */
-    for i in 0..1 {
+    for i in 0..n_threads {
         workers.insert(i, spawn_thread(&search_string));
     }
 
-    give_jobs(&workers, 0, matches.free, recursive);
-    
+    give_jobs(&workers, 0, matches.free, recursive, n_threads);
+
+    for (tx, handler) in workers {
+        drop(tx);
+        handler.join().unwrap();
+    }
 }
